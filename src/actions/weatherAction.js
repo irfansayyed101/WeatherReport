03deca50362e3adda93b5d0c82f632ad154bf9b1
Irfan Sayyed <irfan_sayyed@persistent.co.in>
2020-03-28 18:38:49 +0530

    Action added

--- src/actions/weatherAction.js
+++ src/actions/weatherAction.js
@@ -1,14 +1,20 @@
-    /* Action for saving the weatherlist data  */
+export const WEATHER_LIST = 'WEATHER_LIST';
+export const WEATHER_CHART_LIST = 'WEATHER_CHART_LIST';
+export const WEATHER_CARD_ID = 'WEATHER_CARD_ID';
+export const WEATHER_TEMPERATURE_TYPE = 'WEATHER_TEMPERATURE_TYPE';
+export const NEXT_CARD = 'NEXT_CARD';
+export const PREVIOUS_CARD = 'PREVIOUS_CARD';
+/* Action for saving the weatherlist data  */
 const saveData = (weatherObj) => {
     return {
-        type : 'WEATHER_LIST',
+        type : WEATHER_LIST,
         payload : weatherObj
     }
   }
     /* Action for saving the weatherchart data  */
   const saveChartData = (weatherObj) => {
     return {
-        type : 'WEATHER_CHART_LIST',
+        type : WEATHER_CHART_LIST,
         payload : weatherObj
     }
   }
@@ -16,29 +22,29 @@ const saveData = (weatherObj) => {
     /* Action for displaying the selected card ( day ) in bar chart  */
   const selectedWeathercard = (current) => {
       return{
-          type:'WEATHER_CARD_ID',
-          payload:current
+          type : WEATHER_CARD_ID,
+          payload : current
       }
   }
 
         /* Action for showing the temperature is it Celcius or Fahrenheit  */
   const currentTempratureType = (type) =>{
       return {
-          type : "WEATHER_TEMPERATURE_TYPE",
-          payload:type
+          type : WEATHER_TEMPERATURE_TYPE,
+          payload : type
       }
   }
 
   const nextCard = (type) => {
       return {
-          type : 'NEXT_CARD',
-          payload: type
+          type : NEXT_CARD,
+          payload : type
       }
   }
 
   const previousCard = (type) => {
       return {
-          type : 'PREVIOUS_CARD',
+          type : PREVIOUS_CARD,
           payload : type
       }
   }
