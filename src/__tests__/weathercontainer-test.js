72c281c5d84aa0c04de0cc95ca3806b92ac0741f
Irfan Sayyed <irfan_sayyed@persistent.co.in>
2020-03-28 18:05:34 +0530

    Test

---
+++ src/__tests__/weathercontainer-test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import WeatherContainer from '../container/WeatherContainer';
+import { mount,shallow } from 'enzyme';
+import { Provider } from 'react-redux';
+import {createStore} from 'redux';
+import rootReducer from '../reducers';
+
+describe('<WeatherContainer/> ', () => {
+    it('renders 1 WeatherContainer component', () => {
+        const store = createStore(
+            rootReducer,
+            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() 
+          )
+        const wrapper = shallow(
+            <Provider store={store}>
+                <WeatherContainer />
+            </Provider>
+        );
+         expect(wrapper).toHaveLength(1);
+    });
+});
